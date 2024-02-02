73b36eb5dd928268ca042d889fd806f6c46e8e36
michioxd <michio.haiyaku@gmail.com>
2024-02-02 22:55:32 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,119 @@
+# discord-vugo
+
+<ins>**discord**</ins> <ins>**video**</ins> <ins>**uploader**</ins> written in <ins>**g**</ins>olang. A simple way to host your video on Discord via the HLS method.
+
+## Requirements
+
+- A application in the [Discord Developer Portal](https://discord.com/developers/applications)
+- [FFmpeg](#install-ffmpeg) installed to your `PATH`
+
+## Usage
+
+- Download binaries from [Release page](https://github.com/michioxd/discord-vugo/releases/latest).
+- Open terminal in the folder have `discord-vugo` binary then type `./discord-vugo.exe` (or `chmod +x ./discord-vugo` then `./discord-vugo` in Linux)
+- On the first run, it will create the configuration file `discord-vugo-config.yaml`.
+- Open the `discord-vugo-config.yaml` file with Notepad (or your favorite IDE like `Visual Studio Code`, `Vim`, `nano`,...).
+- You may check out the [Configuration](#configuration) section below.
+
+## Configuration
+
+Use those options in [discord-vugo-config.yaml](discord-vugo-config.yaml.example)
+
+#### `bot_token` [REQUIRED]
+
+**Type**: `string`
+
+**Description:** Bot token, claim it from [Discord Developer Portal](https://discord.com/developers/applications). [How to get bot token](#how-to-get-discord-bot-token)
+
+#### `guild_channel_id` [REQUIRED]
+
+**Type**: `string`
+
+**Description:** Channel ID to upload file into it, right click to a channel you need to get ID, click `Copy Channel ID`. Make sure you add your bot to the server and give your bot permission to send attachments to that channel in your server.
+
+#### `input_file` [REQUIRED]
+
+**Type**: `string`
+
+**Description:** Input file path to upload. Optional if you pass it via `-i` argument.
+
+#### `output_file`
+
+**Type**: `string`
+
+**Default**: `./output.m3u8`
+
+**Description:** Output playlist file path. Can pass it via `-o` argument.
+
+#### `use_proxy`
+
+**Type**: `boolean`
+
+**Default**: `false`
+
+**Description:** Enable proxy URL.
+
+#### `proxy_endpoint`
+
+**Type**: `boolean`
+
+**Description:** Required if `use_proxy` is enabled. Endpoint proxy will write to playlist file. Example with `https://example.com/proxy?url=`, output in the playlist file will be `https://example.com/proxy?url=https%3A%2F%2Fcdn.discordapp.com%2Fattachments%2F1145752139927388313%2F1146790582899978340%2FSuisei_is_Talalala.mp4%3Fex%3D65ce09f5%26is%3D65bb94f5%26hm%3D3ab28624177860467514e24543d7cd34a6a87ed278e06003f679b58052860cde%26`
+
+## Arguments
+
+**Usage:** `discord-vugo <arg> <value>`
+
+**`-i <input file path>`:** Required. Input file path to upload. Optional if you pass it via `input_file` option in configuration file.
+
+**`-o <output file path>`:** Optional. Default `./output.m3u8`. Output playlist file path. Can pass it via `output_file` option in configuration file.
+
+## Tutorial
+
+### How to get Discord Bot Token
+
+- Go to [Discord Developer Portal](https://discord.com/developers/applications)
+- Click `New Application`
+
+  ![image](https://github.com/michioxd/discord-vugo/assets/80969068/326f87bc-8e31-4add-8de8-822cfee7ffdb)
+
+- Enter a name you want (of course bruh), remember to agree Discord TOS :)
+
+  ![image](https://github.com/michioxd/discord-vugo/assets/80969068/5d7d77c2-b421-4b45-8900-7d6d13b4a0f7)
+
+- Go to `Bot` at sidebar
+- Click `Reset Token`
+- Enter 2FA code if you have
+- Copy the new token
+
+  ![image](https://github.com/michioxd/discord-vugo/assets/80969068/4cec19b4-e164-4ab9-b68f-441f4e06ae2c)
+
+### Install FFmpeg
+
+**Windows**
+
+Via Chocolatey
+
+```batch
+choco install ffmpeg
+```
+
+**Linux**
+
+```bash
+# Ubuntu, Debian,...
+sudo apt install ffmpeg
+
+# CentOS
+yum install ffmpeg
+
+# Arch Linux
+pacman -S ffmpeg
+```
+
+## License
+
+[MIT License](LICENSE)
+
+## Credits
+
+[me.](https://github.com/michioxd)
---
+++ main.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/schollz/progressbar/v3"
+	"github.com/spf13/viper"
+)
+
+const version = "0.1"
+const temporaryPath = "./.discord-vugo-tmp"
+
+var fileNameTemp string = "dvugo_" + strconv.FormatInt(time.Now().UnixNano(), 10)
+var ffmpegPath string = ""
+var workingFilePath string = ""
+var outputFilePath string = ""
+
+func init() {
+	fmt.Println("=======\ndiscord-vugo powered by michioxd\nversion: " + version + "\nhttps://github.com/michioxd/discord-vugo\n=======\n ")
+
+	// init config
+	viper.SetConfigFile("./discord-vugo-config.yaml")
+
+	viper.SetDefault("bot_token", "")
+	viper.SetDefault("guild_channel_id", "")
+	viper.SetDefault("file_max_second", "5")
+	viper.SetDefault("use_proxy", false)
+	viper.SetDefault("proxy_endpoint", "")
+
+	if cfgErr := viper.ReadInConfig(); cfgErr != nil {
+		if _, ok := cfgErr.(viper.ConfigFileNotFoundError); ok {
+			fmt.Println("[WARN] Created configuration file, go to discord-vugo-config.yaml then edit something you need")
+			viper.WriteConfigAs("./discord-vugo-config.yaml")
+			os.Exit(0)
+		} else {
+			panic(fmt.Errorf("Failed to read config file: %w", cfgErr))
+		}
+	}
+
+	// check required cfg
+	if viper.GetString("bot_token") == "" {
+		panic(fmt.Errorf("Configuration error. `bot_token` is empty!"))
+	} else if viper.GetString("guild_channel_id") == "" {
+		panic(fmt.Errorf("Configuration error. `guild_channel_id` is empty!"))
+	}
+
+	if viper.GetBool("use_proxy") == true && viper.GetString("proxy_endpoint") == "" {
+		panic(fmt.Errorf("Configuration error. `use_proxy` is enabled but `proxy_endpoint` is empty!"))
+	}
+
+	// check ffmpeg
+	getFfmpegPath, err := exec.LookPath("ffmpeg")
+	if err != nil {
+		panic(fmt.Errorf("Cannot find FFmpeg (ffmpeg), make sure you have installed ffmpeg and add it to your `PATH`!"))
+	} else {
+		ffmpegPath = getFfmpegPath
+	}
+
+	// check input/output
+	flag.StringVar(&workingFilePath, "i", "", "Input video path file. Example: -i \"D:/nggyu.mp4\"")
+	flag.StringVar(&outputFilePath, "o", "./output.m3u8", "Output m3u8 path file. Default ./output.m3u8. Example: -o \"D:/nggyu-hls.m3u8\"")
+
+	if workingFilePath == "" && viper.GetString("input_file") == "" {
+		panic(fmt.Errorf("Please provide input file path argument or in configuration file!"))
+	} else if workingFilePath == "" {
+		workingFilePath = viper.GetString("input_file")
+	}
+
+	if outputFilePath == "" && viper.GetString("output_file") == "" {
+		panic(fmt.Errorf("Output file path you have provided is empty! Please provide output file path argument or in configuration file!"))
+	} else if outputFilePath == "" {
+		workingFilePath = viper.GetString("output_file")
+	}
+
+}
+
+func main() {
+	fmt.Println("Creating session...")
+	bot, botErr := discordgo.New("Bot " + viper.GetString("bot_token"))
+	if botErr != nil {
+		panic(fmt.Errorf("Cannot create session: %w", botErr))
+	}
+
+	fmt.Println("Preparing file...")
+
+	if err := os.Mkdir(temporaryPath, 0755); os.IsExist(err) {
+		os.RemoveAll(temporaryPath)
+		os.Mkdir(temporaryPath, 0755)
+	}
+
+	runFFmpeg := exec.Command(ffmpegPath,
+		"-i", workingFilePath,
+		"-codec:", "copy",
+		"-start_number", "0",
+		"-hls_segment_filename", temporaryPath+"/"+fileNameTemp+"_%03d.ts",
+		"-hls_time", viper.GetString("file_max_second"),
+		"-hls_list_size", "0",
+		"-f", "hls",
+		temporaryPath+"/.discord-vugo-main.m3u8")
+
+	if errors.Is(runFFmpeg.Err, exec.ErrDot) {
+		runFFmpeg.Err = nil
+	}
+
+	if err := runFFmpeg.Run(); err != nil {
+		panic(fmt.Errorf("Error during execute ffmpeg command: %w", err))
+	}
+
+	tmpFiles, err := os.ReadDir(temporaryPath)
+	if err != nil {
+		panic(fmt.Errorf("Cannot read temporary directory: %w", err))
+	}
+
+	playlistFile, err := os.ReadFile(temporaryPath + "/.discord-vugo-main.m3u8")
+	if err != nil {
+		panic(fmt.Errorf("Cannot read playlist file in temporary directory: %w", err))
+	}
+
+	progressUploading := progressbar.NewOptions(len(tmpFiles)-1,
+		progressbar.OptionShowBytes(false),
+		progressbar.OptionSetWidth(15),
+		progressbar.OptionSetDescription("Uploading to Discord..."),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        "=",
+			SaucerHead:    ">",
+			SaucerPadding: " ",
+			BarStart:      "[",
+			BarEnd:        "]",
+		}))
+
+	for _, spilited := range tmpFiles {
+		if path.Ext(spilited.Name()) == ".ts" && strings.Contains(spilited.Name(), fileNameTemp) {
+
+			file, err := os.Open(temporaryPath + "/" + spilited.Name())
+			if err != nil {
+				fmt.Println("Error opening file "+spilited.Name()+":", err)
+			}
+
+			res, err := bot.ChannelFileSend(viper.GetString("guild_channel_id"), spilited.Name(), file)
+			if err != nil {
+				fmt.Println("Error uploading file "+spilited.Name()+":", err)
+				return
+			}
+
+			fileUrl := res.Attachments[0].URL
+
+			if viper.GetBool("use_proxy") == true {
+				fileUrl = viper.GetString("proxy_endpoint") + url.QueryEscape(fileUrl)
+			}
+
+			playlistFile = []byte(strings.Replace(string(playlistFile), spilited.Name(), fileUrl, -1))
+
+			progressUploading.Add(1)
+			file.Close()
+		}
+	}
+
+	file, err := os.Create(outputFilePath)
+	if err != nil {
+		panic(fmt.Errorf("Cannot create output file: %w", err))
+	}
+
+	if _, err := file.Write(playlistFile); err != nil {
+		panic(fmt.Errorf("Error writing to output file: %w", err))
+	}
+
+	fmt.Print("\nCleaning up... ")
+	os.RemoveAll(temporaryPath)
+
+	fmt.Print("OK!")
+
+	bot.Close()
+}
